69ad0c35506c5f6bce9f9b8ad2f2d38500815ab3
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-08-13 23:57:50 +0300

    Basic audio player setup

---
+++ src/Components/AudioPlayer/AudioPlayer.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AudioPlayerContainer = styled.div`
+background-color: red;
+font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+color: aqua;
+font-size: 52px;
+`;
