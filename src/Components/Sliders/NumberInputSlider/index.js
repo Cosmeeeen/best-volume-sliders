0c0855a9f512857898a101fcd502377d4b181c0f
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-08-21 18:57:25 +0300

    Changed first slider to be button based, to improve user inconvenience

--- src/Components/Sliders/NumberInputSlider/index.js
+++ src/Components/Sliders/NumberInputSlider/index.js
@@ -4,13 +4,23 @@ import NumberInputSliderContainer from './index.style';
 
 const NumberInputSlider = ({volume, setVolume}) => {
 
-  const changeVolume = (e) => {
-    setVolume(e.target.value);
-  }
+  const increaseVolume = React.useCallback(() => {
+    if(window.confirm("Are you sure you want to increase the volume?")){
+      setVolume(volume + 1);
+    }
+  }, [volume, setVolume]);
+
+  const decreaseVolume = React.useCallback(() => {
+    if(window.confirm("Are you sure you want to decrease the volume?")){
+      setVolume(volume - 1);
+    }
+  }, [volume, setVolume]);
 
   return (
     <NumberInputSliderContainer>
-      <input type="number" min={0} max={100} defaultValue={volume} onChange={e => changeVolume(e)}/>
+      <h3>Current volume is: {volume}</h3>
+      <button type="button" onClick={increaseVolume}>Increase volume</button>
+      <button type="button" onClick={decreaseVolume}>Decrease volume</button>
     </NumberInputSliderContainer>
   );
 };
--- src/Components/Sliders/NumberInputSlider/index.style.js
+++ src/Components/Sliders/NumberInputSlider/index.style.js
@@ -6,6 +6,24 @@ const NumberInputSliderContainer = styled.div`
   color: ${theme.color.normal};
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  && button{
+    font-size: 16px;
+    padding: 10px;
+    margin: 3px;
+
+    background-color: ${theme.color.normal};
+    color: ${theme.color.background};
+    border: none;
+    border-radius: 5px;
+
+    transition: .3s all;
+    :hover{
+      background-color: ${theme.color.highlight};
+      transform: scale(0.9, 0.9);
+    }
+  }
 `;
 
 export default NumberInputSliderContainer;
