36a8575d6afe5253afdcb6f2f9afc2597b879ccd
Cosmin Ilie <iliemcosmin@yahoo.ro>
2021-10-05 20:16:14 +0300

    Fix audio not playing on mobile (#11)

--- src/Components/AudioPlayer/index.js
+++ src/Components/AudioPlayer/index.js
@@ -1,28 +1,30 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { AudioPlayerContainer } from './index.style';
 import AudioSpinner from '../AudioSpinner/index';
 import audioFile from '../../Audio/verySpecialAudioFile.mp3';
 
 const AudioPlayer = ({ volume }) => {
-  const [playing, setPlaying] = useState(false);
-  const audio = useRef(new Audio(audioFile));
+  const [playing, setPlaying] = React.useState(false);
+  const audio = React.useRef(new Audio(audioFile));
 
-  useEffect(() => {
-    if (playing) {
-      audio.current.play();
-    } else {
-      audio.current.pause();
-    }
-  }, [playing]);
-
-  useEffect(() => {
+  React.useEffect(() => {
+    // On iOS devices, the audio level is always under the user's
+    // physical control. The volume property is not settable in
+    // JavaScript. Reading the volume property always returns 1.
+    // Source: https://apple.co/2Yk5IEK
     audio.current.volume = volume / 100;
-  }, [volume, playing]);
+  }, [audio, volume]);
 
   return (
     <AudioPlayerContainer>
-      <AudioSpinner playing={playing} setPlaying={setPlaying} />
+      <AudioSpinner
+        playing={playing}
+        onClick={() => {
+          playing ? audio.current.pause() : audio.current.play();
+          setPlaying(!playing);
+        }}
+      />
     </AudioPlayerContainer>
   );
 };
